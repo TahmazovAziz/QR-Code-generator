9489f4a8f5355c9293d974beefe603625fb29508
TahmazovAziz <ousernmae@icloud.com>
2025-05-16 23:01:28 +0300

    QRCodegenerateV1

--- src/Components/Input/Input.tsx
+++ src/Components/Input/Input.tsx
@@ -1,4 +1,4 @@
-import './input.scss'
+import './Input.scss'
 interface inputProps{
     value:string;
     InputChange:(e:React.ChangeEvent<HTMLInputElement>)=>void;
@@ -7,7 +7,7 @@ interface inputProps{
 export function Input({value, InputChange}:inputProps){
     return(
         <div className="input-container">
-            <input type="text" value={value} onChange={InputChange} placeholder="Iner url site"/>
+            <input type="text" value={value} onChange={InputChange} placeholder="Enter url site"/>
         </div>
     )
 }
